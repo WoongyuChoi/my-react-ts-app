cd7d9e1367c2746f735cbf96df0381349b53c92a
Schna <schna@kakao.com>
2024-10-14 16:20:14 +0900

    feature: Pretendard 전역 폰트 지정

---
+++ src/theme.ts
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#ff4081",
+    },
+  },
+  typography: {
+    fontFamily: "Prestendard, Arial, sans-serif",
+    allVariants: {
+      fontFamily: "Prestendard, Arial, sans-serif",
+    },
+  },
+});
+
+export default theme;
